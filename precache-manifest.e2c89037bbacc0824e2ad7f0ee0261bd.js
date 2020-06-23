59fbd06f4b20dd93777482031dc718ffd862c625
DimNet <dimkasatin@yandex.ru>
2020-06-23 22:49:48 +0300

    Updates

---
+++ precache-manifest.e2c89037bbacc0824e2ad7f0ee0261bd.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "14e96392bf279b330b80",
+    "url": "./static/css/main.a50d0075.chunk.css"
+  },
+  {
+    "revision": "14e96392bf279b330b80",
+    "url": "./static/js/main.3209492d.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "839f488405586a1e6d45",
+    "url": "./static/css/2.4c093c9e.chunk.css"
+  },
+  {
+    "revision": "839f488405586a1e6d45",
+    "url": "./static/js/2.03741af4.chunk.js"
+  },
+  {
+    "revision": "c53e482c55280e18695f533a6fe47bee",
+    "url": "./static/media/coin.c53e482c.png"
+  },
+  {
+    "revision": "1808b5f6b2dd330757f13c6dc3ddf32a",
+    "url": "./static/media/1233.1808b5f6.ttf"
+  },
+  {
+    "revision": "d8dde2c45fabb1c7700b35203f77be7a",
+    "url": "./static/media/click.d8dde2c4.png"
+  },
+  {
+    "revision": "4e8bd3ea4352428a722a028ba9bc1626",
+    "url": "./index.html"
+  }
+];
